423683da6afdf5c5178aa13d048222948a075191
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-26 12:18:49 +0300

    Add comments

--- helloworld.v
+++ helloworld.v
@@ -4,6 +4,16 @@
  * Compiler SHOULD UNDERSTAND which module is 'main'
  * 'main' module in Verilog = name of Project i.e test
 */
+
+//This code will set button on CPU Cyclone IV to receive signals
+//The button itself is connected to CPU on  98 pin
+//The LED is set to output signal (light), and connected to CPU
+//via pin nr. 100
+
+//All pin nr. that are in use you can see at  Pin Planner 
+//which is located BELOW "Help" upper section and one icon right
+//Then there at the botton you see your X , Y and PIN_nr which
+//it is connected to, then at drawing zoom in to look closely
 module test((* chip_pin = "98" *) input X,
 				(* chip_pin = "100" *) output Y);
 	
